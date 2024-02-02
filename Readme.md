d59a8ac3db62721791db6e91a9120b283b119e1d
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-02-02 17:55:36 -0300

    exercício 2 - gestão de arquivos em javaScript

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-# Exercício - Classes com ECMAScript
+# Exercício 1 - Classes com ECMAScript
 ### Descrição
 1. Deve ter um método "addProduct" que adicionará um produto array produtos
 2. Validar que o campo "código" não se repete e que todos campos são obrigatórios
@@ -7,3 +7,29 @@
 5. Caso nenhum id corresponda, exibir uma mensagem de erro
 
 #### **instalação do pacote: npm install prompt-sync**
+
+# Exercício 2 - Gestão de arquivos em JavaScript
+### Descrição
+1.  Com base no desafio anterior adiconar o fileSystem para alterar modelo de persistência.
+2. A classe ProductManager deve conter os métodos "Adicionar", "Consultar", "Modificar" e "Excluir" um produto e gerenciá-lo em persistência de arquivo.
+3. Uma variável this.path será inicializada a partir do construtor e receber o caminho do arquivo.
+
+## **O exercício em questão de incluir os seguintes requisitos**
+1. addProduct
+    - Método para adicionar um produto.
+    - Atribiu um Id autoincrementado ao produto.
+    - Salva o produto em array no arquivo.
+2. getProducts
+    - Método para obter todos os produtos.
+    - Realiza a leitura do arquivo de produtos.
+    - Retorna produtos em formato array.
+3. getProductsById
+    - Método para obter um produto específico por ID.
+    - Recebe um ID como parâmetro.
+    - Após a leitura do arquivo, localiza e retorna o produto correspondente em formato de objeto.  
+4. updateProduct
+    - Método para atualizar um produto.
+    - Atualiza o produto no arquivo, assegurando que o Id não seja excluído.
+5. deleteProduct 
+    - Método para deletar um produto.
+    - Receber um id e deve deletar o produto com esse id no arquivo.           
--- productManager.js
+++ productManager.js
@@ -1,7 +1,8 @@
-const entrada = require('prompt-sync')({ sigint: true })
-
+const entrada = require('prompt-sync')({ sigint: true });
+const fs = require('fs');
 
 class ProductManager {
+    #pathData = "./data/produto.json"
     constructor(title, description, price, thumbnail, code, stock) {
         this.title = title;
         this.description = description;
@@ -14,13 +15,14 @@ class ProductManager {
     addProduct() {
         let produto = this.readData();
         if (this.checkData(produto)) {
-            productsList.push(produto);
+            this.saveProductToFile(produto);
         }
     }
+
     readData() {
-        let title, description,price, stock;
+        let title, description, price, stock;
         title = entrada("Digite o nome do produto ");
-        description = entrada("Digite o descrição do produto ");
+        description = entrada("Digite a descrição do produto ");
         price = entrada("Digite o preço do produto ");
         stock = entrada("Digite a quantidade de estoque ");
         const product_title = title;
@@ -39,6 +41,7 @@ class ProductManager {
         );
         return produto;
     }
+
     checkData(produto) {
         if (
             produto.title === "" ||
@@ -54,53 +57,102 @@ class ProductManager {
             console.log("Produto válido!");
             return true;
         }
+    }
 
+    saveProductToFile(product) {
+        let productsList = this.readProductsFromFile();
+        productsList.push(product);
+        fs.writeFileSync(this.#pathData, JSON.stringify(productsList));
+        console.log('Produto salvo com sucesso!');
+    }
+
+    readProductsFromFile() {
+        try {
+            const data = fs.readFileSync(this.#pathData, 'utf8');
+            return JSON.parse(data);
+        } catch (err) {
+            return [];
+        }
+    }
+
+
+    upDateProduct() {
+        let productId = entrada("Digite um código de produto para atualizar ");
+        const productsList = this.readProductsFromFile();
+        const index = productsList.findIndex((produto) => productId == produto.code);
+
+        if (index !== -1) {
+            console.log(productsList[index]);
+            let updatedProduct = this.readData();
+            productsList[index] = updatedProduct;
+            fs.writeFileSync(this.#pathData, JSON.stringify(productsList));
+            console.log('Produto atualizado com sucesso!');
+        } else {
+            console.log("PRODUTO NÃO ENCONTRADO");
+        }
+    }
+    deleteProductById() {
+        let productId = entrada("Digite um código para excluir o produto ");
+        let productsList = this.readProductsFromFile();
+        let updatedProductsList = productsList.filter((produto) => productId != produto.code);
+
+        if (updatedProductsList.length < productsList.length) {
+            fs.writeFileSync(this.#pathData, JSON.stringify(updatedProductsList));
+            console.log('Produto excluído com sucesso!');
+        } else {
+            console.log("PRODUTO NÃO ENCONTRADO");
+        }
     }
 
     getProductById() {
-       let productId= entrada("Digite um código para buscar o produto ")
-       const getProduct= productsList.find((produto)=>{
-        return productId==produto.code;
-       })
-
-       if(getProduct){
-        console.log(getProduct)
-       }
-       else{
-        console.log("PRODUTO NÃO ENCONTRADO")
-       }
+        let productId = entrada("Digite um código para buscar o produto ");
+        const productsList = this.readProductsFromFile();
+        const getProduct = productsList.find((produto) => productId == produto.code);
+   
+        if (getProduct) {
+            console.log(getProduct);
+        } else {
+            console.log("PRODUTO NÃO ENCONTRADO");
+        }
     }
 }
 
-const productsList = [];
-const newProduct = new ProductManager();
-const getProduct=new ProductManager();
 let id = 1;
 let teclado;
 let option = 0;
-    while (option != 9) {
-       console.log("*--Digite 1 para registrar produtos--*")
-       console.log("*--Digite 2 para pesquisar produtos--*")
-       console.log("*--Digite 3 para exibir os produtos--*")
-       console.log("*--Digite 9 para sair--*")
-       teclado= entrada(" ESCOLHA UMA OPÇÃO ")
-       option = parseInt(teclado);
+const newProduct = new ProductManager();
 
-    switch (option){
+while (option != 9) {
+    console.log("*--Digite 1 para registrar produtos--*");
+    console.log("*--Digite 2 para pesquisar produtos--*");
+    console.log("*--Digite 3 para exibir os produtos--*");
+    console.log("*--Digite 4 para Atualizar produtos--*");
+    console.log("*--Digite 5 para Excluir produtos--*");
+    console.log("*--Digite 9 para sair--*");
+    teclado = entrada(" ESCOLHA UMA OPÇÃO ");
+    option = parseInt(teclado);
 
+    switch (option) {
         case 1:
             newProduct.addProduct();
             id = id + 1;
             break;
-            case 2: getProduct.getProductById();
+        case 2:
+            newProduct.getProductById();
             break;
-            case 3:
-                if(productsList.length==0){
-                    console.log("AINDA NÃO HÁ PRODUTOS CADASTRADOS")
-                }
-                else{
-                    console.log(productsList);
-                }
-                break;
-       }
-   }
+        case 3:
+            const productsList = newProduct.readProductsFromFile();
+            if (productsList.length == 0) {
+                console.log("AINDA NÃO HÁ PRODUTOS CADASTRADOS");
+            } else {
+                console.log(productsList);
+            }
+            break;
+        case 4:
+            newProduct.upDateProduct();
+            break;
+        case 5:
+            newProduct.deleteProductById();
+            break;
+    }
+}
