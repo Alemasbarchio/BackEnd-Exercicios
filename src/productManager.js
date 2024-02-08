1eaa52ef2c581b288b7dd6a7e27825a4374c059a
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-02-07 21:30:04 -0300

    alteração codigo para utilizar servidorexpress

--- src/productManager.js
+++ src/productManager.js
@@ -1,8 +1,8 @@
-const entrada = require('prompt-sync')({ sigint: true });
-const fs = require('fs');
-let Id=1;
+
+const fs = require('fs').promises;
+
 class ProductManager {
-    #pathData = "../data/produto.json"
+    #pathData = `${__dirname}/../data/produto.json`;
     
      constructor(title, description, price, thumbnail, code, stock) {
         this.title = title;
@@ -14,26 +14,20 @@ class ProductManager {
         
     }
 
-
-    addProduct() {
-        let produto = this.readData();
+    addProduct=async(product)=> {
+       let produto = this.readData(product);
         if (this.checkData(produto)) {
-            produto.code = Id++;
-            this.saveProductToFile(produto);
+          await  this.saveProductToFile(produto);
         }
     }
 
-    readData() {
-        let title, description, price, stock;
-        title = entrada("Digite o nome do produto ");
-        description = entrada("Digite a descrição do produto ");
-        price = entrada("Digite o preço do produto ");
-        stock = entrada("Digite a quantidade de estoque ");
-        const product_title = title;
+    readData(reqBody) {
+        const{title, description, price,stock,thumbnail}=reqBody;
+        const product_title =title;
         const product_desc = description;
         const product_price = price;
-        const product_thumb = "caminho da imagem produto " + Id;
-        const product_code = Id;
+        const product_thumb =thumbnail;
+        const product_code = "";
         const product_stock = stock;
         let produto = new ProductManager(
             product_title,
@@ -42,8 +36,10 @@ class ProductManager {
             product_thumb,
             product_code,
             product_stock
-        );
+        )
         return produto;
+    
+        
     }
 
     checkData(produto) {
@@ -63,17 +59,25 @@ class ProductManager {
         }
     }
 
-    saveProductToFile(product) {
-        let productsList = this.readProductsFromFile();
+    saveProductToFile=async(product)=> {
+        let productsList = await this.readProductsFromFile();
+        const id = productsList.length > 0 ? productsList[productsList.length - 1].code + 1 : 1;
+        product.code=id;
         productsList.push(product);
-        fs.writeFileSync(this.#pathData, JSON.stringify(productsList));
+       await fs.writeFile(this.#pathData, JSON.stringify(productsList));
         console.log('Produto salvo com sucesso!');
     }
 
-    readProductsFromFile() {
+    readProductsFromFile=async()=> {
         try {
-            const data = fs.readFileSync(this.#pathData, 'utf8');
-            return JSON.parse(data);
+            const data = await fs.readFile(this.#pathData, 'utf8');
+            const parsedData = JSON.parse(data);
+    
+            if (Array.isArray(parsedData)) {
+                return parsedData;
+            } else {
+                return [];
+            }
         } catch (err) {
             return [];
         }
@@ -84,81 +88,38 @@ class ProductManager {
         let productId = entrada("Digite um código de produto para atualizar ");
         const productsList = this.readProductsFromFile();
         const index = productsList.findIndex((produto) => productId == produto.code);
-
         if (index !== -1) {
             console.log(productsList[index]);
             let updatedProduct = this.readData();
             updatedProduct.code=parseInt(productId);
             productsList[index] = updatedProduct;
-            fs.writeFileSync(this.#pathData, JSON.stringify(productsList));
+            fs.writeFile(this.#pathData, JSON.stringify(productsList));
             console.log('Produto atualizado com sucesso!');
         } else {
             console.log("PRODUTO NÃO ENCONTRADO");
         }
     }
-    deleteProductById() {
-        let productId = entrada("Digite um código para excluir o produto ");
-        let productsList = this.readProductsFromFile();
-        let updatedProductsList = productsList.filter((produto) => productId != produto.code);
-
+    deleteProductById = async (productId) => {
+        let productsList = await this.readProductsFromFile();
+        let updatedProductsList = productsList.filter((produto) => produto.code != +productId);
+    
         if (updatedProductsList.length < productsList.length) {
-            fs.writeFileSync(this.#pathData, JSON.stringify(updatedProductsList));
+            fs.writeFile(this.#pathData, JSON.stringify(updatedProductsList));
             console.log('Produto excluído com sucesso!');
+            return true; 
         } else {
             console.log("PRODUTO NÃO ENCONTRADO");
+            return false; 
         }
-    }
-
-    getProductById() {
-        let productId = entrada("Digite um código para buscar o produto ");
-        const productsList = this.readProductsFromFile();
-        const getProduct = productsList.find((produto) => productId == produto.code);
-   
-        if (getProduct) {
-            console.log(getProduct);
-        } else {
-            console.log("PRODUTO NÃO ENCONTRADO");
-        }
+    };
+    
+    
+    getProductById= async(productId)=> {
+        const productsList = await this.readProductsFromFile();
+        const getProduct = productsList.find((produto) => +productId === produto.code);
+        return getProduct;
+       
     }
 }
 module.exports=ProductManager;
 
-let teclado;
-let option = 0;
-const newProduct = new ProductManager();
-
-
-while (option != 9) {
-    console.log("*--Digite 1 para registrar produtos--*");
-    console.log("*--Digite 2 para pesquisar produtos--*");
-    console.log("*--Digite 3 para exibir os produtos--*");
-    console.log("*--Digite 4 para Atualizar produtos--*");
-    console.log("*--Digite 5 para Excluir produtos--*");
-    console.log("*--Digite 9 para sair--*");
-    teclado = entrada(" ESCOLHA UMA OPÇÃO ");
-    option = parseInt(teclado);
-
-    switch (option) {
-        case 1:
-            newProduct.addProduct();
-                        
-            break;
-        case 2:
-            newProduct.getProductById();
-            break;
-        case 3:
-            const productsList = newProduct.readProductsFromFile();
-            if (productsList.length == 0) {
-                console.log("AINDA NÃO HÁ PRODUTOS CADASTRADOS");
-            } else {
-                console.log(productsList);
-            }
-            break;
-        case 4:
-            newProduct.upDateProduct();
-            break;
-        case 5:
-            newProduct.deleteProductById();
-            break;
-    }
-}
