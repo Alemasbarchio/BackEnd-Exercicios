89c54e283488ceb77fe7f52219c5301d3f2e706d
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-03-18 23:35:39 -0300

    ajustes das rotas para novo modelo de persistencia

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,87 +1,128 @@
-import express from"express";
+import express from "express";
 const productRouter = express.Router();
-import productManager from"../productManager.js";
-const pm = new productManager();
-
-
-
-productRouter.get("/", async (req, res) => {
-    try {
-        const productsList = await pm.readProductsFromFile();
-        
-        let limit = req.query.limit;
-        
-        if (limit) {
-           const limitedList = productsList.slice(0, +limit);
-           return res.render("home", { title: "Lista de Produtos", limitedList});
-        } else {
-           return res.render("home", { title: "Lista de Produtos", productsList});
-          
-       }
-    } catch (error) {
-        console.error("Erro ao carregar dados do arquivo JSON:", error);
-        res.status(500).json({ error: "Erro ao consultar produtos" });
-    }
-});
-
-productRouter.get("/products/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        const searchProduct = await pm.getProductById(id);
-        if (searchProduct) {
-             res.render("home", { title: "Lista de Produtos", searchProduct});
-            
-        } else {
-            res.render("home", {error:"teste" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Erro " });
-    }
-
-})
-
-
+//import productManager from"../productManager.js";
+//const pm = new productManager();
+import productService from "../dao/service/product-service.js";
 
 
 productRouter.get("/realtimeproducts", async (req, res) => {
     try {
-        const productsList = await pm.readProductsFromFile();
-        res.render("products", { title: "Produtos em Tempo Real", productsList });
-      
+        const product = await productService.getProducts();
+        res.render("products", { title: "Produtos em Tempo Real", product });
+
+
     } catch (error) {
-        console.error("Erro ao carregar dados do arquivo JSON:", error);
         res.status(500).json({ error: "Erro ao consultar produtos" });
+
     }
-});
 
-
-
+})
 
 productRouter.post("/realtimeproducts", async (req, res) => {
+    const product = req.body;
+    await productService.creatProduct(product);
+    res.redirect("/realtimeproducts")
+})
+
+productRouter.delete("/realtimeproducts/:code", async (req, res) => {
+    const { code } = req.params;
+    await productService.deleteProduct(code);
+    res.redirect("/realtimeproducts")
+});
+
+
+productRouter.put("/realtimeproducts/:pcode", async (req, res) => {
+    const { pcode } = req.params;
+    const productData = req.body;
+    console.log(pcode);
     try {
-        const { title, description, price, thumbnail, code, stock } = req.body;
-        await pm.readData(req.body);
-        await pm.addProduct(req.body);
-        const productsList = await pm.readProductsFromFile();
-        res.render("products", { title: "Produtos em Tempo Real", productsList });
+        const updatedProduct = await productService.upDateproduct(productData, pcode);
+        res.status(200).json(updatedProduct);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Erro ao cadastrar produto" });
+        console.error('Erro ao atualizar o produto:', error);
+        res.status(500).json({ error: "Erro ao atualizar o produto." });
     }
 });
 
 
-productRouter.delete("/realtimeproducts/:id", async (req, res) => {
-    const id = req.params.id;
-  
-    try {
-      await pm.deleteProductById(id);
-      res.status(201).json({ message: "Produto deletado com sucesso" });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Erro ao deletar produto." });
-    }
-  });
+// #region modelo persistencia FileSystem exer 3 & 4
+// productRouter.get("/", async (req, res) => {
+
+//     try {
+//         const productsList = await pm.readProductsFromFile();
+//         let limit = req.query.limit;
+
+//         if (limit) {
+//            const limitedList = productsList.slice(0, +limit);
+//            return res.render("home", { title: "Lista de Produtos", limitedList});
+//         } else {
+//            return res.render("home", { title: "Lista de Produtos", productsList});
+
+//        }
+//     } catch (error) {
+//         console.error("Erro ao carregar dados do arquivo JSON:", error);
+//         res.status(500).json({ error: "Erro ao consultar produtos" });
+//     }
+// });
+
+// #region Modelo persistencia FileSystem exer 3 & 4
+// productRouter.get("/products/:id", async (req, res) => {
+//     const { id } = req.params;
+//     try {
+//         const searchProduct = await pm.getProductById(id);
+//         if (searchProduct) {
+//              res.render("home", { title: "Lista de Produtos", searchProduct});
+
+//         } else {
+//             res.render("home", {error:"teste" });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ error: "Erro " });
+//     }
+
+// })
+
+// #region Modelo persistência FileSystem exer 3 & 4
+// productRouter.get("/realtimeproducts", async (req, res) => {
+
+//     try {
+//         const productsList = await pm.readProductsFromFile();
+//         res.render("products", { title: "Produtos em Tempo Real", productsList });
+
+//     } catch (error) {
+//         console.error("Erro ao carregar dados do arquivo JSON:", error);
+//         res.status(500).json({ error: "Erro ao consultar produtos" });
+//     }   
+// });
+
+// #end
+
+
+// #region Modelo persistência FileSystem exer 3 & 4
+// productRouter.post("/realtimeproducts", async (req, res) => {
+//     try {
+//         const { title, description, price, thumbnail, code, stock } = req.body;
+//         await pm.readData(req.body);
+//         await pm.addProduct(req.body);
+//         const productsList = await pm.readProductsFromFile();
+//         res.render("products", { title: "Produtos em Tempo Real", productsList });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: "Erro ao cadastrar produto" });
+//     }
+
+// });
+
+// #region Modelo persistência FileSystem exer 3 & 4
+//  productRouter.delete("/realtimeproducts/:id", async (req, res) => {
+//      const id = req.params.id;
+
+//     try {
+//       await pm.deleteProductById(id);
+//       res.status(201).json({ message: "Produto deletado com sucesso" });     } catch (error) {
+//        console.log(error);
+//        res.status(500).json({ error: "Erro ao deletar produto." });     }
+//    });
 
 export default productRouter;
