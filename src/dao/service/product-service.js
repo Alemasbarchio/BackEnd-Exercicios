462f88f7869252ad4d9626c43091111fd2f4abf1
Alemasbarchio <ale.qbexbarbosa@gmail.com>
2024-03-23 17:11:56 -0300

    atualizações metodos para persistencia

--- src/dao/service/product-service.js
+++ src/dao/service/product-service.js
@@ -1,36 +1,59 @@
 import productModel from "../models/product-model.js";
 
-
+const getProductById = async (code) => {
+  try {
+      const product = await productModel.findOne({ code: code });
+      if (product) {
+          const productJSON = product.toJSON();
+          return productJSON;
+      } else {
+          return null;
+      }
+  } catch (error) {
+      console.log(error);
+      throw error; 
+  }
+};
 
 
 const getProducts = async () => {
-  let products = await productModel.find();
+
+  try {
+    let products = await productModel.find();
   products = products.map((product) => product.toJSON());
   return products;
-
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 
 const creatProduct = async (product) => {
-  let productCreated = await productModel.create(product)
+  try {
+    let productCreated = await productModel.create(product)
   return productCreated;
-
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 
 const deleteProduct = async (code) => {
-  const product = await productModel.deleteOne({ code: code });
 
-  if (product.deletedCount == 1) {
+  try {
+    const product = await productModel.deleteOne({ code: code });
+    console.log(product);
     return product;
+         
+  } catch (error) {
+    console.log(error);
   }
-  else {
-    console.log("produto nao existe")
-  }
-
+   
 };
 
-const upDateproduct = async (product, pcode) => {
-  const productUpdate = await productModel.updateOne({ code: pcode }, { product });
+const upDateproduct = async (productData, code) => {
+  const productUpdate = await productModel.updateOne({ code: code },  productData);
+  console.log(productUpdate);
   return productUpdate
 }
-export default { getProducts, creatProduct, deleteProduct, upDateproduct };
+export default { getProducts, creatProduct, deleteProduct, upDateproduct,getProductById };
